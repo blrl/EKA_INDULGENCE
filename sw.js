c50ecbb31b7bf0d7e3b4aae84bbd2cdec92a1ce3
blrl <buckyvibes1@gmail.com>
2026-02-08 05:56:30 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,25 @@
-const CACHE_NAME = 'eka-cache-v1';
-const assets = ['/', '/index.html', '/logo.png'];
+const CACHE_NAME = 'eka-indulgence-v1';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  './logo.png'
+];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
+// Install service worker
+self.addEventListener('install', evt => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      cache.addAll(assets);
+    })
   );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(res => res || fetch(event.request))
+// Fetching content
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(cacheRes => {
+      return cacheRes || fetch(evt.request);
+    })
   );
 });
